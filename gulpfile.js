5f883d8497f4fb6b118c271ff7b662c999ffb210
viktor <va.fursenko@yandex.ru>
2017-02-07 17:07:50 +0300

    Flags relocated, gulpfile started

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,47 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
-    livereload = require('gulp-livereload'),
     vueify = require('gulp-vueify'),
-    webpack = require('webpack-stream'),
+    babel = require('gulp-babel');
     fs = require('fs');
 
 var srcPath = './src',
-    distPath = './dist';
+    libPath = '../node_modules',
+    distPath = './dist',
+    examplePath = './example/';
 
-gulp.task('js', function () {
+// Component pack
+gulp.task('component', function () {
+    gulp.src([
+        srcPath + '/img/**/*'
+    ])
+    .pipe(gulp.dest(distPath + '/img'));
+
     return gulp.src([
         srcPath + '/**/*.js',
         srcPath + '/**/*.vue'
     ])
     .pipe(vueify())
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(uglify({
+        compress: {
+            drop_debugger: false
+        }
+    }))
+    .pipe(concat('vue-country-select.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(distPath));
+});
+
+// JS pack
+gulp.task('js', function () {
+    // Custom js
+    gulp.src([
+        examplePath + '/js/*.js'
+    ])
     .pipe(sourcemaps.init())
     .pipe(uglify({
         compress: {
@@ -25,8 +52,24 @@ gulp.task('js', function () {
     .on('error', function (err) {
         console.log(err);
     })
-    .pipe(concat('vue-country-select.js'))
+    .pipe(concat('app.js'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(distPath))
-    .pipe(livereload());
+    .pipe(gulp.dest(examplePath + '/assets/js'));
+
+
+    // Libs js
+    gulp.src([
+        libPath + '/vue/dist/vue.min.js',
+        distPath + '/vue-country-select.js',
+    ])
+    .pipe(concat('lib.js'))
+    .pipe(gulp.dest(examplePath + '/assets/js'));
+});
+
+// Flags images
+gulp.task('img', function () {
+    gulp.src([
+        distPath + '/img/**/*'
+    ])
+    .pipe(gulp.dest(examplePath + '/assets/img'));
 });
